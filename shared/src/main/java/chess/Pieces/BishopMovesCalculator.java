f8caf6af1f73c1ca4a8d8af833cab5586d5a84e6
YoSoyLechuga97 <strongc97@gmail.com>
2024-01-19 16:16:37 -0700

    Refractored the piece classes and added the 'Moves Calculator'

---
+++ shared/src/main/java/chess/Pieces/BishopMovesCalculator.java
@@ -0,0 +1,7 @@
+package chess.Pieces;
+
+public class BishopMovesCalculator {
+    //Needs to be able to move in any diagonal until they are blocked
+    //Returns a collection of possible moves for the piece
+
+}
