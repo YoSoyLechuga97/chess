6a40317421363ac9042e454d698de6bb21f9e69e
YoSoyLechuga97 <strongc97@gmail.com>
2024-01-22 11:30:37 -0700

    Added equals and hash functions for ChessBoard, ChessPiece, ChessPosition and ChessMove

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -55,5 +56,18 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         PiecesMovesCalculator allMoves = new PiecesMovesCalculator(board, myPosition, getPieceType());
         return allMoves.pieceMoves();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && pieceType == that.pieceType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, pieceType);
     }
 }
