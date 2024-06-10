c93de141317879f237617cddd0a0628a527069ff
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 17:39:51 -0600

    Implemented getUser in MemoryuserDAO.java

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.UserData;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MemoryUserDAO implements UserDAO{
 
@@ -20,6 +21,18 @@ public class MemoryUserDAO implements UserDAO{
 
     @Override
     public UserData getUser(String username) {
-        return null;
+        int userIndex = -1;
+        //Search through the array for the matching username
+        for (int i = 0; i < users.size(); i++) {
+            if (Objects.equals(users.get(i).username(), username)) {
+                userIndex = i;
+            }
+        }
+        //Return userData if there, if not, return null
+        if (userIndex >= 0) {
+            return users.get(userIndex);
+        } else {
+            return null;
+        }
     }
 }
