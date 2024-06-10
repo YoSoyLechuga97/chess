33624742c3a00ae41ba03a3859a855a604720b59
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 17:35:32 -0600

    Implemented memory version of createUser and clear

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -2,15 +2,24 @@ package dataaccess;
 
 import model.UserData;
 
+import java.util.ArrayList;
+
 public class MemoryUserDAO implements UserDAO{
+
+    static final ArrayList<UserData> users = new ArrayList<>();
 
     @Override
     public void clear() {
-
+        users.clear();
     }
 
     @Override
     public void createUser(UserData userData) {
+        users.add(userData);
+    }
 
+    @Override
+    public UserData getUser(String username) {
+        return null;
     }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,8 +1,10 @@
 package dataaccess;
 
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
 
 public interface UserDAO {
     public void clear();
     public void createUser(UserData userData);
-}
+    public UserData getUser(String username);
+    }
