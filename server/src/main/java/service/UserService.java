7a8dc891c59132d15ebb8e255974bb3d70975b3d
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 20:24:52 -0600

    Passoff tests for clear and login

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,16 +1,16 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.DataAccessException;
-import dataaccess.UserDAO;
+import dataaccess.*;
 import model.AuthData;
 import model.UserData;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class UserService {
-    UserDAO userDAO;
-    AuthDAO authDAO;
+    UserDAO userDAO = new MemoryUserDAO();
+    AuthDAO authDAO = new MemoryAuthDAO();
+    GameDAO gameDAO = new MemoryGameDAO();
     public AuthData register(UserData user) throws DataAccessException {
         if (userDAO.getUser(user.username()) == null) {
             userDAO.createUser(user);
@@ -20,11 +20,16 @@ public class UserService {
     }
     public AuthData login(UserData user) throws DataAccessException {
         if (userDAO.getUser(user.username()) != null) {
-            if (userDAO.getUser(user.username()).password() == user.password()) {
+            if (Objects.equals(userDAO.getUser(user.username()).password(), user.password())) {
                 return authDAO.createAuth(user.username());
             }
         }
         return null;
     }
     public void logout(UserData user) {};
+    public void clear() throws DataAccessException {
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+    }
 }
--- server/src/test/java/service/myTests.java
+++ server/src/test/java/service/myTests.java
@@ -1,21 +1,56 @@
 package service;
 
+import dataaccess.DataAccessException;
+import model.AuthData;
 import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import service.UserService;
 import org.eclipse.jetty.server.Authentication;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.crypto.Data;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class myTests {
-    @BeforeAll
+    @BeforeEach
     //Create my Users
-    UserData user1 = new UserData("newGuy", "newGuyPassword", "newGuyEmail@yahoo.com");
-    UserData user2 = new UserData("HiFriend", "HiFriendPassword", "HiFriend@gmail.com");
-    UserData user3 = new UserData("Gamer44", "Gamer44Password", "Gamer44@aol.com");
+    public void setup() throws DataAccessException{
+        UserData user1 = new UserData("newGuy", "newGuyPassword", "newGuyEmail@yahoo.com");
+        UserData user2 = new UserData("HiFriend", "HiFriendPassword", "HiFriend@gmail.com");
+        UserData user3 = new UserData("Gamer44", "Gamer44Password", "Gamer44@aol.com");
+        UserService myService = new UserService();
+        myService.register(user1);
+        myService.register(user2);
+        myService.register(user3);
+    }
 
     @Test
     @DisplayName("Clear Users")
-    public void clearUsers() {
+    public void clearUsers() throws DataAccessException {
+        UserService myService = new UserService();
+        myService.clear();
+    }
 
+    @Test
+    @DisplayName("Login Attempt")
+    public void logginIn() throws DataAccessException {
+        UserService myService = new UserService();
+        System.out.println("Using correct login information");
+        UserData login1 = new UserData("newGuy", "newGuyPassword", "newGuyEmail@yahoo.com");
+        AuthData actual1 = myService.login(login1);
+        if (actual1 == null) {
+            System.out.println("FAILED TO LOGIN");
+        }
+
+        System.out.println("Using Incorrect information");
+        UserData login2 = new UserData("newGuy", "newGuyPasswordWrong", "newGuyEmail@yahoo.com");
+        AuthData actual2 = myService.login(login2);
+        if (actual2 != null) {
+            System.out.println("FAILED TO STOP LOGIN");
+        }
     }
 }
