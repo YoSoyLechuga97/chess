a8f11d9dfeaaea4ec52650b8c4ad18e9e76e97f3
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 18:06:54 -0600

    Established getGame() method in MemoryGameDAO.java

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,35 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MemoryGameDAO implements GameDAO{
+    ArrayList<GameData> games = new ArrayList<>();
+    @Override
+    public int createGame(String authToken, String gameName) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    //Returns GameID if game exists, -1 if cannot find game
+    public int getGame(String gameName) throws DataAccessException {
+        for (int i = 0; i < games.size(); i++) {
+            if (Objects.equals(games.get(i).gameName(), gameName)) {
+                return games.get(i).gameID();
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public ArrayList<GameData> listGames() throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public GameData updateGame(GameData updatedGame) throws DataAccessException {
+        return null;
+    }
 }
