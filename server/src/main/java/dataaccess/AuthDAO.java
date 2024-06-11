c72e32de3256316c60bbf6ac7baba619922f11bd
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 18:48:29 -0600

    createAuth() in MemeoryAuthDAO.java

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public interface AuthDAO {
     ArrayList<AuthData> auths = new ArrayList<>();
     public void clear() throws DataAccessException;
-    public AuthData createAuth() throws DataAccessException;
+    public AuthData createAuth(String username) throws DataAccessException;
     public boolean getAuth(String token) throws DataAccessException;
     public void deleteAuth(String token) throws DataAccessException;
 
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -2,6 +2,8 @@ package dataaccess;
 
 import model.AuthData;
 
+import java.security.SecureRandom;
+
 public class MemoryAuthDAO implements AuthDAO{
     @Override
     public void clear() throws DataAccessException {
@@ -9,8 +11,23 @@ public class MemoryAuthDAO implements AuthDAO{
     }
 
     @Override
-    public AuthData createAuth() throws DataAccessException {
-        return null;
+    public AuthData createAuth(String username) throws DataAccessException {
+        //Generate a random authentication token
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] tokenBytes = new byte[8 / 2];
+        secureRandom.nextBytes(tokenBytes);
+        StringBuilder sb = new StringBuilder();
+        for (byte b : tokenBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        String newToken = sb.toString();
+
+        //Create new data model
+        AuthData newAuth = new AuthData(newToken, username);
+
+        //Add to list
+        auths.add(newAuth);
+        return newAuth;
     }
 
     @Override
