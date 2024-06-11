766500928c26f2757b46ba1da1c48d0ba5fac66b
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 18:27:09 -0600

    createGame() method in MemoryGameDAO.java

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,15 +1,25 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
 
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Random;
 
 public class MemoryGameDAO implements GameDAO{
     ArrayList<GameData> games = new ArrayList<>();
     @Override
     public int createGame(String authToken, String gameName) throws DataAccessException {
-        return 0;
+        //Check to make sure game doesn't already exist:
+        if (getGame(gameName) == -1) { //Game with this name doesn't currently exist
+            //Generate random gameID
+            Random random = new Random();
+            int newGameID = 10000000 + random.nextInt(90000000);
+            GameData newGame = new GameData(newGameID, "NO USER", "NO USER", "gameName", new ChessGame());
+            return newGameID;
+        }
+        return -1;
     }
 
     @Override
