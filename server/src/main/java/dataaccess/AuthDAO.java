b7ddc2c6deeb3e2d1e6cf87935a508946d18185a
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 18:38:16 -0600

    AuthDAO.java created

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,15 @@
+package dataaccess;
+
+import model.AuthData;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public interface AuthDAO {
+    ArrayList<AuthData> auths = new ArrayList<>();
+    public void clear() throws DataAccessException;
+    public AuthData createAuth() throws DataAccessException;
+    public boolean getAuth(String token) throws DataAccessException;
+    public void deleteAuth(String token) throws DataAccessException;
+
+}
--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -10,17 +10,17 @@ public class MemoryUserDAO implements UserDAO{
     static final ArrayList<UserData> users = new ArrayList<>();
 
     @Override
-    public void clear() {
+    public void clear() throws DataAccessException {
         users.clear();
     }
 
     @Override
-    public void createUser(UserData userData) {
+    public void createUser(UserData userData) throws DataAccessException {
         users.add(userData);
     }
 
     @Override
-    public UserData getUser(String username) {
+    public UserData getUser(String username) throws DataAccessException {
         int userIndex = -1;
         //Search through the array for the matching username
         for (int i = 0; i < users.size(); i++) {
