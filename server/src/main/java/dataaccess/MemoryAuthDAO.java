9f7c862d16ed131e3de1cf99f5c11ce89059b78f
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-10 18:39:05 -0600

    clear() in MemoryAuthDAO.java

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,25 @@
 package dataaccess;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+
+public class MemoryAuthDAO implements AuthDAO{
+    @Override
+    public void clear() throws DataAccessException {
+        auths.clear();
+    }
+
+    @Override
+    public AuthData createAuth() throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public boolean getAuth(String token) throws DataAccessException {
+        return false;
+    }
+
+    @Override
+    public void deleteAuth(String token) throws DataAccessException {
+
+    }
 }
