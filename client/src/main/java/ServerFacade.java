41588552e508cb3028c29c52aac389e1c2a149f1
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-15 12:41:36 -0600

    Created login method in sf

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,26 +1,40 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
 
+import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import server.Server;
 
 public class ServerFacade {
     int port;
+    String path;
+    String method;
+    String body;
+    String url;
     private static Server server;
     public ServerFacade() throws DataAccessException {
-        try {
-            this.port = server.run(0);
-        } catch (DataAccessException e) {
-            throw new RuntimeException(e);
-        }
+        this.port = server.run(0);
+    }
+
+    public void login(String username, String password) throws URISyntaxException, IOException {
+        path = "session";
+        url = createURL(path);
+        body = "{ \"username\":\"" + username + "\", \"password\":\"" + password + "\" }";
+        method = "POST";
+
+        HttpURLConnection loginConnection = sendRequest(url, method, body);
+        readResponseBody(loginConnection);
     }
 
     //Connect Helper Functions
-    public URI createURI(String path) throws URISyntaxException {
-        return new URI("http://localhost:" + port + "/" + path);
+    public String createURL(String path) throws URISyntaxException {
+        return "http://localhost:" + port + "/" + path;
     }
     private static HttpURLConnection sendRequest(String url, String method, String body) throws URISyntaxException, IOException, IOException {
         URI uri = new URI(url);
@@ -38,5 +52,21 @@ public class ServerFacade {
                 outputStream.write(body.getBytes());
             }
         }
+    }
+    private static void receiveResponse(HttpURLConnection http) throws IOException {
+        var statusCode = http.getResponseCode();
+        var statusMessage = http.getResponseMessage();
+
+        Object responseBody = readResponseBody(http);
+        System.out.printf("= Response =========\n[%d] %s\n\n%s\n\n", statusCode, statusMessage, responseBody);
+    }
+
+    private static Object readResponseBody(HttpURLConnection http) throws IOException {
+        Object responseBody = "";
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            responseBody = new Gson().fromJson(inputStreamReader, Map.class);
+        }
+        return responseBody;
     }
 }
