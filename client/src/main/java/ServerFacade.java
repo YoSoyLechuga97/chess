4cc8683572535cc8fd79ee47901396acb83035bc
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-15 12:17:15 -0600

    Generated URI helper function

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,9 +1,14 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class ServerFacade {
     int port;
     public ServerFacade(int port) {
         this.port = port;
     }
 
-    //Connect Helper Function
-
+    //Connect Helper Functions
+    public URI createURI(String path) throws URISyntaxException {
+        return new URI("http://localhost:" + port + "/" + path);
+    }
 }
