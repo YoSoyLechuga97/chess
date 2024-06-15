c5838940052a124db34982a4edbb7cd312a640c9
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-15 12:27:12 -0600

    sendRequest and write request body in sf

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -18,5 +21,22 @@ public class ServerFacade {
     //Connect Helper Functions
     public URI createURI(String path) throws URISyntaxException {
         return new URI("http://localhost:" + port + "/" + path);
+    }
+    private static HttpURLConnection sendRequest(String url, String method, String body) throws URISyntaxException, IOException, IOException {
+        URI uri = new URI(url);
+        HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+        http.setRequestMethod(method);
+        writeRequestBody(body, http);
+        http.connect();
+        System.out.printf("= Request =========\n[%s] %s\n\n%s\n\n", method, url, body);
+        return http;
+    }
+    private static void writeRequestBody(String body, HttpURLConnection http) throws IOException {
+        if (!body.isEmpty()) {
+            http.setDoOutput(true);
+            try (var outputStream = http.getOutputStream()) {
+                outputStream.write(body.getBytes());
+            }
+        }
     }
 }
