ddd0b70b3ad2a7205f413425aa5c1606aee22886
YoSoyLechuga97 <strongc97@gmail.com>
2024-06-15 21:59:47 -0600

    Blocking out test code

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,14 +1,14 @@
-import chess.ChessGame;
-import chess.ChessPiece;
-import server.Server;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
-        Server server = new Server();
-        var port = server.run(0);
-        TerminalUI.run(port);
-    }
-}
+//import chess.ChessGame;
+//import chess.ChessPiece;
+//import server.Server;
+//
+//public class Main {
+//    public static void main(String[] args) throws Exception {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+//        Server server = new Server();
+//        var port = server.run(0);
+//        TerminalUI.run(port);
+//    }
+//}
 
